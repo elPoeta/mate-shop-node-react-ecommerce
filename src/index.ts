2a8b5a329f0ffb150ec4a0e7901cd5409621c437
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-01-30 20:12:05 -0300

    config express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("OK!")
+import express, { Request, Response } from "express";
+import cors from 'cors';
+
+const PORT: string = process.env.PORT || '5000';
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: "Hello from server" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server ran on port ${PORT}`);
+});
