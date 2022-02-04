9e64d87ddc3ec7caff42489730bbfafefe4f62da
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-02-03 22:14:58 -0300

    add auth controller - register user and save to db

---
+++ server/src/controllers/authController.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import asyncHandler from "../middlewares/asyncHandler";
+import { UserI } from "../interfaces/user";
+import User from '../models/user';
+
+export const register = asyncHandler(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const body = req.body as Pick<UserI, "email" | "password">;
+  const user: UserI = new User({
+    email: body.email,
+    password: body.password,
+    isAdmin: false
+  });
+  await user.save();
+  const { email, isAdmin, ...rest } = user;
+  res.status(201).json({
+    message: 'User registred',
+    email,
+    isAdmin
+  });
+});
