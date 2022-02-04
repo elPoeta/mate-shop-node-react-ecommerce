0a4d1db5dd4715de62cb07433dc171c7f56d1c4f
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-02-03 21:29:53 -0300

    add error handler middleware

--- server/src/setUpApp/routes.ts
+++ server/src/setUpApp/routes.ts
@@ -1,6 +1,7 @@
 import { Application } from "express";
 import users from '../routes/users';
-
+import { errorHandler } from "../middlewares/errorMiddleware";
 export const routes = (app: Application) => {
   app.use('/api/v1/users', users);
+  app.use(errorHandler);
 } 
