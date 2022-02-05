4db3b99c7e756aabd5e89fe958b26efe290401a4
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-02-05 19:25:40 -0300

    add erro response class - refactor error handler

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -2,11 +2,18 @@ import { NextFunction, Request, Response } from "express";
 import asyncHandler from "@middlewares/asyncHandler";
 import { User } from "@interfaces/user";
 import { AuthService } from "@service/authService";
+import { ErrorResponse } from "@utils/errorRespnse";
 
 export const register = asyncHandler(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const body = req.body as Pick<User, "email" | "password" | "isAdmin" | "confirmPassword">;
+  if (!body.password?.length || !body.confirmPassword?.length || body.confirmPassword !== body.password) {
+    res.status(400);
+    throw new ErrorResponse('Error password not match', 400);
+  }
+
   const user: User = await AuthService.createNewUser(body);
   res.status(201).json({
+    statusCode: 201,
     message: 'User registred',
     id: user.ID,
     email: user.email,
--- server/src/middlewares/errorMiddleware.ts
+++ server/src/middlewares/errorMiddleware.ts
@@ -1,4 +1,7 @@
 import { NextFunction, Request, Response } from "express"
+import { ErrorResponse } from "@utils/errorRespnse";
+import { Error as MongooseError, } from "mongoose";
+import { MongoError } from 'mongodb';
 
 const notFound = (req: Request, res: Response, next: NextFunction): void => {
   const error = new Error(`Not Found - ${req.originalUrl}`)
@@ -7,12 +10,34 @@ const notFound = (req: Request, res: Response, next: NextFunction): void => {
 }
 
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction): void => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  let error = { ...err };
+
+  error.message = err.message;
+
+  console.log("error ", err);
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  if (err.name === 'CastError') {
+    const message = `Resource not found`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  if ((err as MongoError).code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  if (err instanceof MongooseError.ValidationError) {
+    const message = Object.values(err.errors).map(val => val.message).join(', ');
+    error = new ErrorResponse(message, 400);
+  }
+
   res.status(statusCode)
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-  })
+    .json({
+      message: err.message,
+      statusCode,
+      stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    })
 }
 
 export { notFound, errorHandler }
