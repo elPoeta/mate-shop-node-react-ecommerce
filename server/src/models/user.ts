97daff10f17d593bb44ab590d9fd2781f8ded8c2
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-02-05 18:33:53 -0300

    hash password

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,4 +1,5 @@
 import { model, Schema } from "mongoose";
+import * as bcrypt from 'bcryptjs';
 import { UserDocument } from '@interfaces/user';
 
 const UserSchema: Schema = new Schema(
@@ -32,5 +33,17 @@ const UserSchema: Schema = new Schema(
   },
   { timestamps: true }
 );
+
+UserSchema.methods.matchPassword = async function (enteredPassword: string) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 export default model<UserDocument>("User", UserSchema);
