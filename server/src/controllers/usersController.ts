d65cd2189513f542a34ea8a6f06212fc0c238440
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-02-04 23:19:11 -0300

    refactor scripts json package

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -3,5 +3,5 @@ import asyncHandler from "@middlewares/asyncHandler";
 
 export const getUsers = asyncHandler(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 
-  res.status(200).json([{ email: 'leonardo.a.tosetto@gmail.com' }]);
+  res.status(200).json([{ email: 'leonardo.a.tosetto@gmail.com', isAdmin: true }]);
 });
