3d3ee3cc1b0a238ac4ef4945fd5c5aebc3be9f41
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-02-07 21:42:17 -0300

    add product model

---
+++ server/src/models/product.ts
@@ -0,0 +1,62 @@
+import { model, Schema } from "mongoose";
+import { ProductDocument } from '@interfaces/product';
+
+const ProductSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      max: 5,
+      min: 0,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0,
+    },
+    discount: {
+      type: Number,
+      min: 0,
+      max: 100,
+      default: 0
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model<ProductDocument>('Product', ProductSchema);
+
