6de13a73cce3066beacc9f64ba3edfdc71fad391
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-02-07 21:58:38 -0300

    create product routes

---
+++ server/src/controllers/productController.ts
@@ -0,0 +1,6 @@
+import { NextFunction, Request, Response } from 'express';
+import asyncHandler from '@middlewares/asyncHandler';
+
+export const geProducts = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json([]);
+});
--- server/src/setupApp/routes.ts
+++ server/src/setupApp/routes.ts
@@ -1,8 +1,10 @@
 import { Application } from "express";
 import user from '@routes/users';
+import product from '@routes/product';
 import { errorHandler } from "@middlewares/errorMiddleware";
 
 export const routes = (app: Application) => {
   app.use('/api/v1/user', user);
+  app.use('/api/v1/product', product);
   app.use(errorHandler);
 } 
