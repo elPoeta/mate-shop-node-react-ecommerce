97f4fdf20275383b897956e1588d8d3916b985ee
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-02-16 11:26:27 -0300

    fix ts config file

--- client/src/features/counter/counterAPI.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fetchCount = void 0;
-function fetchCount(amount = 1) {
-    return new Promise((resolve) => setTimeout(() => resolve({ data: amount }), 500));
-}
-exports.fetchCount = fetchCount;
