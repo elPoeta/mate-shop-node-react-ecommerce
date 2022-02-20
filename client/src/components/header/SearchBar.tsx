9b9ab9342024cc52385a9d9f9ced9c1546e66ab3
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-02-20 18:41:22 -0300

    style sarch bar component

--- client/src/components/header/SearchBar.tsx
+++ client/src/components/header/SearchBar.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { SearchIcon } from '@heroicons/react/solid';
 
 const SearchBar: React.FC = (): JSX.Element => {
   return (
-    <div>
+    <div className='header_search_bar'>
       <input type='search' placeholder='search in mate shop' />
+      <button ><SearchIcon className='header_search_bar_icon' /></button>
     </div>
   )
 }
