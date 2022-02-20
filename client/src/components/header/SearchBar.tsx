e5b72e8ac36d6b9f8bff92d019b49ddfb950d517
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-02-20 18:10:31 -0300

    style header brand

---
+++ client/src/components/header/SearchBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBar: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <input type='search' placeholder='search in mate shop' />
+    </div>
+  )
+}
+
+export default SearchBar
