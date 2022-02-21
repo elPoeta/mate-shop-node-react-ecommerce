b35c55098a75a6a243705290457474bbea8e8f00
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-02-21 14:31:38 -0300

    add auth reducer

--- client/src/features/auth/authService.ts
+++ client/src/features/auth/authService.ts
@@ -6,8 +6,19 @@ const login = async (userData: LoginFormData) => {
   const response = await axios.post(AUTH_URL, userData);
 
   if (response.data) {
+    console.log("RESPONSE ", response.data)
     localStorage.setItem('user', JSON.stringify(response.data));
   }
 
   return response.data;
 }
+
+const logout = () => {
+  localStorage.removeItem('user')
+}
+
+const authService = {
+  logout,
+  login,
+}
+export default authService;
