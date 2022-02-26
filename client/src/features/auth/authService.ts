f5febb0559999672e6bfb455c2dabb1ce103687c
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-02-26 17:51:29 -0300

    decode token on login'

--- client/src/features/auth/authService.ts
+++ client/src/features/auth/authService.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { AUTH_URL } from "../../utils/apiUrls";
 import { LoginFormData } from "../../interfaces/loginFormData";
+import jwtDecode from "jwt-decode";
 
 const login = async (userData: LoginFormData) => {
   const response = await axios.post(`${AUTH_URL}/login`, userData);
@@ -8,6 +9,8 @@ const login = async (userData: LoginFormData) => {
   if (response.data) {
     console.log("RESPONSE ", response.data)
     localStorage.setItem('user', JSON.stringify(response.data));
+    const jwt = jwtDecode(response.data.token);
+    console.log("JWT", jwt)
   }
 
   return response.data;
