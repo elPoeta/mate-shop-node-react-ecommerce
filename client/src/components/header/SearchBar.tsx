af47f76d57935e22766bbae274215bdb8724aa5b
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-04-05 08:36:18 -0300

    style navbar with tailwind

--- client/src/components/header/SearchBar.tsx
+++ client/src/components/header/SearchBar.tsx
@@ -3,9 +3,9 @@ import { SearchIcon } from '@heroicons/react/solid';
 
 const SearchBar: React.FC = (): JSX.Element => {
   return (
-    <div className='col-span-2'>
-      <input type='search' placeholder='search in mate shop' />
-      <button ><SearchIcon className='h-5 w-5' /></button>
+    <div className=' flex items-center justify-center col-span-3 py-2'>
+      <input type='search' placeholder='search in mate shop' className='text-lg border rounded-full w-auto focus:outline-none focus:border-emerald-300' />
+      <button ><SearchIcon className='h-8 w-8' /></button>
     </div>
   )
 }
