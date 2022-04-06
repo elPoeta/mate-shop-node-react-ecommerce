5519de010c33bdd1240eb86c776d64059e522cae
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-04-06 08:39:48 -0300

    style for desktop

--- client/src/components/header/SearchBar.tsx
+++ client/src/components/header/SearchBar.tsx
@@ -3,9 +3,11 @@ import { SearchIcon } from '@heroicons/react/solid';
 
 const SearchBar: React.FC = (): JSX.Element => {
   return (
-    <div className=' flex items-center justify-center col-span-3 py-2'>
-      <input type='search' placeholder='search in mate shop' className='text-lg border rounded-full w-auto focus:outline-none focus:border-emerald-300' />
-      <button ><SearchIcon className='h-8 w-8' /></button>
+    <div className='col-span-3 py-2 md:col-start-2 md:col-span-2 md:row-start-1'>
+      <div className='flex items-center justify-center'>
+        <input type='search' placeholder='search in mate shop' className='text-lg border rounded-full w-auto focus:outline-none focus:border-emerald-300 md:w-[100%]' />
+        <button ><SearchIcon className='h-8 w-8' /></button>
+      </div>
     </div>
   )
 }
